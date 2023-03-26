4406e56d803d3f09a68b6fd2a8716a9c5e796f9a
Alla <urt2509@gmail.com>
2023-03-26 13:50:40 +0200

    update task5

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const inputName = document.querySelector("#name-input");
+
+const outputName = document.querySelector("#name-output");
+
+inputName.addEventListener("input", onInput);
+
+function onInput(evt) {
+  evt.currentTarget.value === ""
+    ? (outputName.textContent = "Anonymous")
+    : (outputName.textContent = evt.currentTarget.value);
+}
