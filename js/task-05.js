4be0093b7086e6ac029f2fb4cf803e019dfbd6b1
Alla <urt2509@gmail.com>
2023-03-27 22:40:56 +0200

    updated tasks 5 and 6

--- js/task-05.js
+++ js/task-05.js
@@ -5,7 +5,7 @@ const outputName = document.querySelector("#name-output");
 inputName.addEventListener("input", onInput);
 
 function onInput(evt) {
-  evt.currentTarget.value === ""
+  evt.currentTarget.value.trim() === ""
     ? (outputName.textContent = "Anonymous")
     : (outputName.textContent = evt.currentTarget.value);
 }
