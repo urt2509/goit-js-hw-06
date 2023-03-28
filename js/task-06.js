054964f369f9ec3d66c4e89199dde41148258b3a
Alla <urt2509@gmail.com>
2023-03-28 10:27:02 +0200

    updated task6

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,14 @@ const inputText = document.querySelector("#validation-input");
 inputText.addEventListener("blur", onValidate);
 
 function onValidate(evt) {
-  evt.currentTarget.value.replace(/\s/g, "").length === 6
-    ? inputText.classList.add("valid")
-    : inputText.classList.add("invalid");
+  evt.currentTarget.value.trim().length === 6
+    ? inputText.classList.toggle("valid")
+    : inputText.classList.toggle("invalid");
 }
+
+// ---------------regular expression without whitespaces etc.
+// function onValidate(evt) {
+//   evt.currentTarget.value.replace(/\s/g, "").length === 6
+//     ? inputText.classList.add("valid")
+//     : inputText.classList.add("invalid");
+// }
