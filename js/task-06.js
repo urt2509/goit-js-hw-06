92dbe7393d2aa4d283d5197c5fede7d187fb22e3
Alla <urt2509@gmail.com>
2023-04-03 12:05:00 +0200

    updated task6

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,13 @@
 const inputText = document.querySelector("#validation-input");
 
 inputText.addEventListener("blur", onValidate);
+console.dir(inputText);
 
-function onValidate(evt) {
-  evt.currentTarget.value.trim().length === 6
-    ? inputText.classList.toggle("valid")
-    : inputText.classList.toggle("invalid");
-}
+// function onValidate(evt) {
+//   evt.currentTarget.value.trim().length === 6
+//     ? inputText.classList.toggle("valid")
+//     : inputText.classList.toggle("invalid");
+// }
 
 // ---------------regular expression without whitespaces etc.
 // function onValidate(evt) {
@@ -14,3 +15,22 @@ function onValidate(evt) {
 //     ? inputText.classList.add("valid")
 //     : inputText.classList.add("invalid");
 // }
+
+function onValidate(evt) {
+  const expectedLength = inputText.dataset.length;
+  console.log(expectedLength);
+  console.log(typeof expectedLength);
+
+  const inputLength = evt.currentTarget.value.trim().length;
+  console.log("inputed length is " + inputLength);
+
+  if (inputLength === Number(expectedLength)) {
+    inputText.classList.remove("invalid");
+    inputText.classList.add("valid");
+    // console.dirinput("inputed text if invalid: " + inputText.classList);
+  } else {
+    inputText.classList.remove("valid");
+    inputText.classList.add("invalid");
+    // console.dir("inputed text: " + inputText.classList);
+  }
+}
